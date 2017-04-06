73ef5864fcb3e3570f0b2ada3b5c36abde38c8e2
Joe Hand <joe@hand.email>
2017-04-06 13:22:37 -0700

    use neat log for nicer output

--- cli.js
+++ cli.js
@@ -2,19 +2,41 @@
 
 var path = require('path')
 var prettyBytes = require('prettier-bytes')
+var neatLog = require('neat-log')
+var output = require('neat-log/output')
 var count = require('.')
 
 var dir = process.argv[2] || process.cwd()
+var neat = neatLog(view)
+neat.use(doCount)
 
-console.log('Counting files and directories in...')
-console.log(path.resolve(dir))
-count(dir, function (err, stats) {
-  if (err) {
-    console.error(err)
-    process.exit(1)
-  }
-  console.log('\nResults:')
-  console.log(' Files: ', stats.files)
-  console.log(' Dirs:  ', stats.dirs)
-  console.log(' Size:  ', prettyBytes(stats.bytes))
-})
+function view (state) {
+  if (!state.stats) return 'Hello! Let me see what you have here.'
+  return output`
+    Counting files and directories in...
+    ${path.resolve(dir)}
+
+    Files: ${state.stats.files}
+    Dirs: ${state.stats.dirs}
+    Size: ${prettyBytes(state.stats.bytes)}
+  `
+}
+
+function doCount (state, bus) {
+  state.stats = count(dir, function (err, stats) {
+    if (err) {
+      console.error(err)
+      process.exit(1)
+    }
+    state.stats = stats
+    neat.render()
+    console.log('\nAll done!')
+    process.exit(0)
+  })
+  setInterval(function () {
+    // Print updated stats
+    // TODO: could check for changes?
+    bus.emit('render')
+  }, 100)
+  bus.emit('render')
+}
